64a6c315a25026ff2f20868fed7063a371ba9682
Alonso CT <g.alonso.ct@gmail.com>
2024-05-17 12:08:35 -0600

    solve CORS issue

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -11,7 +11,11 @@ const Home: React.FC = () => {
   useEffect(() => {
     const fetchBanks = async () => {
       try {
-        const response = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks');
+        const response = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks', {
+          headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+          },
+        });
         const data = await response.json();
         setBanks(data);
         setLoading(false);
