8ca453b739ba728a26ecda74db97b29884271a7a
Alonso CT <g.alonso.ct@gmail.com>
2024-05-17 15:32:19 -0600

    catch error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import BankList from '@/components/BankList';
-import { Bank } from '@/types/bank';
-import styles from '@/styles/Home.module.scss';
 
-async function getBanks(): Promise<Bank[]> {
+async function getBanks() {
   try {
     const response = await fetch('https://dev.obtenmas.com/catom/api/challenge/banks', {
       cache: "no-store",
     });
-
-    if (!response.ok) {
-      console.error('No hay respuest del servidor:', response.status, response.statusText);
-      return [];
-    }
-
     return await response.json();
-  } catch (error) {
-    console.error('Error al cargar los datos:', error);
+  }
+  catch(error) {
+    console.log('FETCH ERROR:')
+    console.error(error);
     return [];
   }
 }
@@ -24,13 +18,5 @@ async function getBanks(): Promise<Bank[]> {
 export default async function Home() {
   const banks = await getBanks();
 
-  if (banks.length === 0) {
-    return (
-      <div className={styles.error_message}>
-        <p>Error al cargar los datos</p>
-      </div>
-    );
-  }
-
-  return <BankList banks={banks} />;
+  return <BankList banks={banks} />
 };
